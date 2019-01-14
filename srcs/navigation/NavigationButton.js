99788f3097d93a2fd352c71c94946eea5bb9d129
Valentin Tolochko <vtolochk@gmail.com>
2019-01-14 16:47:23 +0200

    [UPD] rounded button for footer

--- srcs/navigation/NavigationButton.js
+++ srcs/navigation/NavigationButton.js
@@ -11,6 +11,16 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center', 
 		alignItems: 'center'
+	},
+	plusButton: {
+		alignSelf: 'center', 
+		position: 'absolute', 
+		elevation: 4, 
+		height: 65, 
+		width: 65, 
+		bottom: 5, 
+		borderRadius: 35, 
+		justifyContent: 'center'
 	}
 })
 
@@ -23,12 +33,22 @@ class NavigationButton extends React.PureComponent {
 
 	render() {
 		const { text, icon, path, activeRoute } = this.props
-		return (
-			<Button vertical active={activeRoute === path} onPress={this.onButtonPress}>
-				<View style={styles.buttonContent}><Icon name={icon}/></View>
-				{text && <Text numberOfLines={1} style={styles.text}>{text}</Text>}
-			</Button>
-		)
+
+		if (!text) {
+			return (<View style={styles.buttonContent}>
+				<Button style={styles.plusButton} onPress={this.onButtonPress} active>
+					<Icon name={icon}/>
+				</Button>
+			</View>)
+		}
+		else {
+			return (	
+				<Button vertical active={activeRoute === path} onPress={this.onButtonPress}>
+					<View style={styles.buttonContent}><Icon name={icon}/></View>
+					{text && <Text numberOfLines={1} style={styles.text}>{text}</Text>}
+				</Button>
+			)
+		}
 	}
 }
 
