e4bce506cf70f130b52904fd12c1d9e5d76d81c5
Valentin Tolochko <vtolochk@gmail.com>
2019-01-15 21:18:11 +0200

    [UPD] screenHeader component

--- srcs/components/ScreenHeader/ScreenHeader.js
+++ srcs/components/ScreenHeader/ScreenHeader.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { StyleSheet } from 'react-native'
 import { Header,Button, Icon, Left, Body, Right, Title } from 'native-base'
+
+const styles = StyleSheet.create({
+	headerBody: {
+		flex: 1, 
+		alignItems:'center'
+	},
+	flex: {
+		flex: 1
+	},
+	title: {
+		minWidth: 100
+	}
+})
 
 class ScreenHeader extends React.PureComponent {
 	render() {
@@ -11,14 +26,20 @@ class ScreenHeader extends React.PureComponent {
 		
 		return (
 			<Header>
-				{ withBackButton ? withButton : <Left style={{flex: 1}}/> }
-				<Body style={{flex: 1, alignItems:'center'}}>
-					<Title>{title}</Title>
+				{ withBackButton ? withButton : <Left style={styles.flex}/> }
+				<Body style={styles.headerBody}>
+					<Title style={styles.title}>{title}</Title>
 				</Body>
-				<Right style={{flex: 1}} />
+				<Right style={styles.flex} />
 			</Header>
 		)
 	}
+}
+
+ScreenHeader.propTypes = {
+	history: PropTypes.object,
+	title: PropTypes.string,
+	withBackButton: PropTypes.bool
 }
 
 export default ScreenHeader
