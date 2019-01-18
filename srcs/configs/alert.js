6845c27257cc43f9795a4b586b3ffc87031c4aca
Valentin Tolochko <vtolochk@gmail.com>
2019-01-18 15:50:17 +0200

    [UPD] alert update

--- srcs/configs/alert.js
+++ srcs/configs/alert.js
@@ -1,9 +1,12 @@
 import { Alert } from 'react-native'
 
-export const alert = (title, message) => Alert.alert(
+export const alert = (title = '', message = '', okFunc, cancelFunc = () => {}) => Alert.alert(
 	title,
 	message,
-	[ { text: 'OK', onPress: () => {} } ],
+	[ 
+		{ text: 'OK', onPress: okFunc },
+		{ text: okFunc ? 'Cancel' : '', onPress: cancelFunc}
+	],
 	{ cancelable: false }
 )
 
