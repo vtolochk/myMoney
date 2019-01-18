63a9fab761d1bb81427c43adbce30225836a9f57
Valentin Tolochko <vtolochk@gmail.com>
2019-01-18 15:51:55 +0200

    [UPD] transactions component

--- srcs/components/Transactions/Transactions.js
+++ srcs/components/Transactions/Transactions.js
@@ -1,15 +1,54 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Text, View } from 'react-native'
+import { List, Text } from 'native-base'
+import { ADD_MONEY_PATH } from '@navigation'
+import { TransactionItem } from '@components'
+import { withRouter } from 'react-router-native'
+import { ScrollView, View, StyleSheet } from 'react-native'
 
-const Transactions = (props) => {
-	return (<View style={{flex: 1, flexDirection: 'column'}}>
-		{props.transactions.map((e, i) => <Text key={i}>{e.sum} | {e.type} | {e.date.toString()}</Text>)}
-	</View>)
+const styles = StyleSheet.create({
+	center: {
+		padding: 15,
+	},
+	text: {
+		textAlign: 'center',
+		color: 'grey'
+	}
+})
+
+class Transactions extends React.PureComponent{
+
+	onPressItem = (id) => {
+		this.props.history.push(
+			{ 
+				pathname: ADD_MONEY_PATH,
+				state: { id }
+			})
+	}
+
+	render() {
+		if (!this.props.transactions.length)
+			return <View style={styles.center}><Text style={styles.text}>There are no transactions yet.</Text></View>
+		return (
+			<ScrollView>
+				<List>
+					{this.props.transactions.map((trans, i) => (
+						<TransactionItem
+							key={i}
+							{...trans}
+							category={this.props.categories[trans.categoryId]}
+							onPressItem={() => this.onPressItem(i)}
+							date={trans.date.toString().substr(4, 12)}
+						/>))}
+				</List>
+			</ScrollView>
+		)
+	}
 }
 
 const mapStateToProps = state => ({
+	categories: state.categoriesReducer.categories,
 	transactions: state.transactionReducer.transactions
 })
 
-export default connect(mapStateToProps)(Transactions)
+export default withRouter(connect(mapStateToProps)(Transactions))
