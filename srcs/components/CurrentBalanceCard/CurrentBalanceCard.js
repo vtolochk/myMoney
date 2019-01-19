e0a3d800fb3293c33fb8f18155c2006482be1f4a
Valentin Tolochko <vtolochk@gmail.com>
2019-01-19 10:04:09 +0200

    [ADD] current balance card component

---
+++ srcs/components/CurrentBalanceCard/CurrentBalanceCard.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Card, CardItem, Text } from 'native-base'
+
+const styles = StyleSheet.create({
+	text: {
+		fontSize: 20,
+		textAlign: 'center'
+	},
+	textContainer: {
+		flex: 1,
+		flexDirection: 'column',
+	},
+	positiveStyle: {
+		color: 'green'
+	},
+	negativeStyle: {
+		color: 'red'
+	}
+})
+
+class CurrentBalanceCard extends React.PureComponent {
+    render() {
+		const { isPositive, balance } = this.props
+		const textStyle = isPositive ? styles.positiveStyle : styles.negativeStyle
+        return (
+            <Card>
+			<CardItem style={styles.textContainer}>
+                <Text style={{...styles.text, ...textStyle}}>{balance}</Text>
+                <Text>Current balance</Text>
+			</CardItem>
+			</Card>
+        )
+    }
+}
+
+export default CurrentBalanceCard
