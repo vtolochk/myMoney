49eaa816d483f017ec55ea0a9b07672b67bb31be
Valentin Tolochko <vtolochk@gmail.com>
2019-01-20 20:47:12 +0200

    [UPD] storage refactoring

--- srcs/redux/store/index.js
+++ srcs/redux/store/index.js
@@ -1,14 +1,15 @@
 import { createStore } from 'redux'
+import storage from 'redux-persist/lib/storage'
 import rootReducer from '../reducers/rootReducer'
 import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+
 
 const persistConfig = {
-    key: 'myMoney',
-    storage,
-  }
+	key: 'myMoneyStore',
+	storage,
+}
   
-  const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
   
-  export const store = createStore(persistedReducer)
-  export const persistor = persistStore(store)
+export const store = createStore(persistedReducer)
+export const persistor = persistStore(store)
