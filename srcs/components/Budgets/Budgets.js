ccb456e2b1247dda417e27dbdfbc9f1c772a8ecb
Valentin Tolochko <vtolochk@gmail.com>
2019-01-20 20:44:00 +0200

    [UPD] budget component

--- srcs/components/Budgets/Budgets.js
+++ srcs/components/Budgets/Budgets.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text } from 'native-base'
+import { StyleSheet, View } from 'react-native'
 
-const Budgets =() => <Text>Budgets</Text>
+const styles = StyleSheet.create({
+	view: {
+		padding: 15,
+	},
+	text: {
+		textAlign: 'center',
+		color: 'grey'
+	}
+})
+
+const Budgets =() => <View style={styles.view}><Text style={styles.text}>Coming soon!</Text></View>
 
 export default Budgets
